29b04b5e45cabc2e1fa188d8d9dbca20a3f967d8
abraham <aaabrham421@gmail.com>
2023-11-04 16:45:36 +0300

    feat: add api crud and auth service functions

---
+++ src/services/api.tsx
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { env } from "../utils/env";
+
+const BASE_URL = env.VITE_API_URL;
+
+export const get = async (url: string, params?: any) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/${url}`,
+      params ? { withCredentials: true, params } : { withCredentials: true }
+    );
+    return response.data;
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
+    throw error;
+  }
+};
+
+export const create = async (url: string, data: any) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/${url}`, data, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
+    throw error;
+  }
+};
+
+export const update = async (url: string, data: any) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/${url}`, data, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
+    throw error;
+  }
+};
+
+export const remove = async (url: string) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/${url}/`, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
+    throw error;
+  }
+};
+
+export const upload = async (
+  url: string,
+  formData: FormData,
+  onUploadProgress: (e: any) => void
+) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/${url}`, formData, {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+    return response.data;
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
+    throw error;
+  }
+};
+
+function handleStatusCodes(status: number) {
+  switch (status) {
+    case 401:
+      window.location.replace("/auth/login");
+  }
+}
---
+++ src/services/auth.ts
@@ -0,0 +1,24 @@
+export const setUserId = (id: string) => {
+    localStorage.setItem("userId", JSON.stringify(id));
+  };
+  
+  export const getUserId = () => {
+    return localStorage.getItem("userId")
+      ? JSON.parse(localStorage.getItem("userId") ?? "")
+      : null;
+  };
+  
+  export const setUser = (user: any) => {
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+  
+  export const getUser = () => {
+    return localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user") ?? "")
+      : null;
+  };
+  
+  export const isLoggedIn = () => {
+    return getUserId() !== null && getUser() !== null;
+  };
+  
