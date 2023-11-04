2ba55235e0f7c4383e168c4301722045b1a0ccce
abraham <aaabrham421@gmail.com>
2023-11-04 18:37:44 +0300

    feat: add layout file with header, sidebar, footer and breadcrumb

---
+++ src/layout/BreadCrumb.tsx
@@ -0,0 +1,17 @@
+export default function BreadCrumb() {
+  return (
+    <>
+      <div className="pagetitle">
+        <h1>Dashboard</h1>
+        <nav>
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="index.html">Home</a>
+            </li>
+            <li className="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </nav>
+      </div>
+    </>
+  );
+}
