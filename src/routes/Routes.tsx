be6eba5706d8bbdba1df76ee9457f2e2e414ae65
abraham <aaabrham421@gmail.com>
2023-11-04 16:44:54 +0300

    feat: removed template code, configured routing

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,52 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { isLoggedIn } from "../services/auth";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/auth/Login";
+import Dashboard from "../pages/dashboard/Dashboard";
+
+export default function Router() {
+  // TODO: uncomment the following line after adding auth
+  // let loggedIn = isLoggedIn();
+  let loggedIn = true;
+
+  useEffect(() => {
+    // TODO: uncomment the following line after adding auth
+    // loggedIn = isLoggedIn();
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="auth/">
+            <Route index element={<Navigate to="/login" />} />
+            <Route path="login/">
+              <Route index element={<Login />} />
+              {/* <Route path="successfull" element={<SuccessCallback />} /> */}
+            </Route>
+          </Route>
+          <Route path="/" element={<Navigate to="/dashboard" />}></Route>
+
+          {/* Private Routes */}
+          {loggedIn ? (
+            <>
+              <Route
+                path="/dashboard"
+                element={
+                  // <MainLayout>
+                  <Dashboard />
+                  // </MainLayout>
+                }
+              />
+
+              <Route path="*" element={<NotFound />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/auth/login" replace />} />
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
