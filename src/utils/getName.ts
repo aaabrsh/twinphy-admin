acf3f56b96d087a71b302cd320cce2be676bf44a
abraham <aaabrham421@gmail.com>
2023-11-05 19:50:31 +0300

    feat: add new util functions

---
+++ src/utils/getName.ts
@@ -0,0 +1,17 @@
+export const getName = (user: any) => {
+    if (!user) {
+      return "loading ... ";
+    }
+  
+    let res = "";
+    if (user.first_name) {
+      res += user.first_name;
+    }
+  
+    if (user.last_name) {
+      res += " " + user.last_name;
+    }
+  
+    return res;
+  };
+  
