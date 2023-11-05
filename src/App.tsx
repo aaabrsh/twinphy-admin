734bb49afd26cb0d36706bcaed51dec69b208674
abraham <aaabrham421@gmail.com>
2023-11-05 19:49:54 +0300

    chore: installed packages, import necessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Router from "./routes/Routes";
