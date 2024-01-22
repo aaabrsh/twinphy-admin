547ff375d2f835808c05fa81df9a1fcd2dd239fe
abraham <aaabrham421@gmail.com>
2024-01-22 18:15:12 +0300

    fix: fix header logo and text

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -48,10 +48,14 @@ export default function Header() {
         className="header fixed-top d-flex align-items-center"
       >
         <div className="d-flex align-items-center justify-content-between">
-          <a href="index.html" className="logo d-flex align-items-center">
-            <img src="/assets/img/logo.png" alt="" style={{maxHeight: "41px"}} />
+          <Link to="/" className="logo d-flex align-items-center">
+            <img
+              src="/assets/img/logo.png"
+              alt=""
+              style={{ maxHeight: "41px" }}
+            />
             <span className="d-none d-lg-block">VidiBattle Admin</span>
-          </a>
+          </Link>
           <i
             onClick={toggleSideBar}
             className="bi bi-list toggle-sidebar-btn"
